f63e6d8e5e40ee188b62f295e4a1fa0dfa72c4b3
Rishit Kumar <rishitkumar@Rishits-MacBook-Air.local>
2024-12-25 17:21:14 +0530

    Initial commit

---
+++ src/main/java/com/hello/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.hello.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
